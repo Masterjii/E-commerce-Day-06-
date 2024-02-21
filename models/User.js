ddd8aa3958416229e0249af5a72b6f6941f7eae6
Balram Kusharam <balramkushram120@gmail.com>
2024-02-21 19:33:25 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,44 @@
+
+const mongoose = require('mongoose');   //object
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+// Schema -
+const userSchema = new mongoose.Schema({
+
+//    username - PL (passport-local)
+//    password - PLM (passport-local-mongoose)
+
+   email:{
+    type:String,
+    trim:true,
+    required:true
+   },
+   role:{
+    type:String,
+    trim:true,
+    default: 'buyer'
+   },
+   gender:{
+    type:String,
+    trim:true,
+    required:true
+   }
+})
+
+userSchema.plugin(passportLocalMongoose);  // always apply on schema
+
+let User = mongoose.model('User', userSchema);
+module.exports = User;
+
+
+
+
+
+
+
+
+
+
+
+
